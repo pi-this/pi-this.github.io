b22ea19868cf119056b35d72c1866f4bfe9ee6b0
pi-this <114669697+pi-this@users.noreply.github.com>
2022-10-07 16:30:55 -0400

    Update Cpicoins.js

--- javascript/Cpicoins.js
+++ javascript/Cpicoins.js
@@ -1,5 +1,6 @@
 localStorage.setItem("pi-coins", 0);
 function addcoins() {
   var coins = localStorage.getItem("pi-coins");
-  localStorage.setItem("pi-coins", coins+1);
+  var coins += 5
+  localStorage.setItem("pi-coins", coins);
 }
