7257d9180bc68b87492718097adf24de025106e7
pi-this <114669697+pi-this@users.noreply.github.com>
2023-08-11 12:40:27 -0400

    Delete Cpicoins.js

--- javascript/Cpicoins.js
+++
@@ -1,7 +0,0 @@
-localStorage.setItem("pi-coins", 0);
-function addcoins() { 
-  var coins = localStorage.getItem("pi-coins");
-  coins = coins + 1
-  localStorage.setItem("pi-coins", coins);
-  alert(coins)
-}
